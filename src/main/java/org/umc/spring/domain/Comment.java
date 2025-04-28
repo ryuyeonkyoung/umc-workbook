6815a7d67803cefc683ef5d56fbc4e64813e4211
류연경 <ykr.dev@gmail.com>
2025-04-28 17:46:12 +0900

    feat: 대댓글 기능을 위한 Comment 엔티티 수정
    - 1-depth 대댓글 구조 (자기 참조 방식)
    - parent 필드에 @JsonIgnore 적용 (순환 참조 방지)
    - children 리스트에 @OrderBy("createdAt ASC") 설정 (정렬)

--- src/main/java/org/umc/spring/domain/Comment.java
+++ src/main/java/org/umc/spring/domain/Comment.java
@@ -1,9 +1,13 @@
 package org.umc.spring.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import org.umc.spring.domain.common.BaseEntity;
 import org.umc.spring.domain.enums.CommentStatus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -25,4 +29,18 @@ public class Comment extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private CommentStatus status;
+
+    //  대댓글 기능을 위한 자기참조 연관관계 설정
+
+    // 부모 댓글을 참조 (null이면 최상위 댓글)
+    // parent_id 외래키 생성
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id") // 부모 댓글 ID를 저장하는 외래키
+    @JsonIgnore // 순환 참조 방지 //TODO: DTO에서도 순환 구조 주의 필요
+    private Comment parent;
+
+    // 자식 댓글 목록  (1-depth)
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    @OrderBy("createdAt ASC") // 등록한 순서대로 정렬
+    private List<Comment> children = new ArrayList<>();
 }
